8db036212a3f76820e9e394804c5808b68d6831e
Joao Ricardo Teixeira <joricteixeira@gmail.com>
2019-08-11 15:49:31 +0100

    adding chef boostrap extension

--- deploy.ps1
+++ deploy.ps1
@@ -1,13 +1,23 @@
 params(
     [string]$AccountName,
     [string]$AccountPassword,
-    [string]$ResourceGroupName
+    [string]$ResourceGroupName,
+
+    [string]$ChefServerUrl,
+    [string]$ChefValidationClientName,
+    [string]$ChefValidationKey
 )
 
-#Preferencialmente utilizar um Service Principal, entretanto devido à impossibilidade de se realizar alteracoes no AzureAD, estou utilizando minha própria conta
+#Preferencialmente utilizar um Service Principal, entretanto devido a impossibilidade de se realizar alteracoes no AzureAD, estou utilizando minha propria conta
 $password = ConvertTo-SecureString "$($AccountPassword)" -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential($AccountName, $password)
 
 Login-AzAccount -Credential $credential
 
-New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile .\azuredeploy.json -TemplateParameterFile .\azuredeploy.parameters.json
+New-AzureRmResourceGroupDeployment `
+    -ResourceGroupName $ResourceGroupName `
+    -TemplateFile .\azuredeploy.json `
+    -TemplateParameterFile .\azuredeploy.parameters.json `
+    -chefServerUrl $ChefServerUrl `
+    -chefValidationClientName $ChefValidationClientName `
+    -chefValidationKey $ChefValidationKey
