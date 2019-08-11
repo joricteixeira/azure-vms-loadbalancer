4dfc0b332ec2adb64114b3e86aa24930f3729dfc
Joao Ricardo Teixeira <joricteixeira@gmail.com>
2019-08-11 20:02:43 +0100

    Flexibilizando adiccao de parametros externos

--- deploy.ps1
+++ deploy.ps1
@@ -3,24 +3,29 @@ param(
     [string]$AccountPassword,
     [string]$ResourceGroupName,
 
-    [string]$ChefServerUrl,
-    [string]$ChefValidationClientName,
-    [string]$ChefValidationKey
+    [hashtable]$ExtraParameters
 )
 
 #Preferencialmente utilizar um Service Principal, entretanto devido a impossibilidade de se realizar alteracoes no AzureAD, estou utilizando minha propria conta
 $password = ConvertTo-SecureString "$($AccountPassword)" -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential($AccountName, $password)
 
-$overrideParameters = @{}
-$overrideParameters.Add("chefServerUrl",$ChefServerUrl)
-$overrideParameters.Add("chefValidationClientName",$ChefValidationClientName)
-$overrideParameters.Add("chefValidationKey",$ChefValidationKey)
+#Hack para unir parametros oriundos do template com parametros externos oriundos do buildserver ou etc
+$params = Get-Content .\azuredeploy.parameters.json | ConvertFrom-Json
+$templateParameter = @{}
+foreach($p in $params.parameters.PSObject.Properties){
+    if($p.Value.value -eq $null -or $p.Value.value -eq ""){
+        Write-Host "Pulando parametro [$($p.Name)]"
+    }else{
+        Write-Host "Adicionando parametro [$($p.Name)]"
+        $templateParameter.Add($p.Name,$p.Value.value)
+    }
+}
+$templateParameter += $ExtraParameters
 
 Login-AzureRmAccount -Credential $credential
 
 New-AzureRmResourceGroupDeployment `
     -ResourceGroupName $ResourceGroupName `
     -TemplateFile .\azuredeploy.json `
-    -TemplateParameterFile .\azuredeploy.parameters.json `
-    -TemplateParameterObject $overrideParameters
+    -TemplateParameterObject $templateParameter
